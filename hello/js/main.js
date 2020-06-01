c1ed93202fe41f388bb51af53ed83ccfb93c3016
Tomoyuki Irikawa <t.irikawa@adseed.co.jp>
2020-06-01 22:44:38 +0900

    Create:ToDoApp

---
+++ hello/js/main.js
@@ -0,0 +1,19 @@
+var app = new Vue({
+  el: '#app',
+  data: {
+    message: 'Hello World!',
+    url: 'https://jp.vuejs.org/',
+    toggle: true,
+    languages: [
+      'JavaScript', 
+      'Ruby', 
+      'Python'
+    ]
+  },
+  methods: {
+    clickHandler: function() {
+      this.message = 'Clicked!'
+    }
+  }
+})
+x
